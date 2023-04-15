41893c427b3420b27babe296e910135f2f1e6f18
Abed Hijazi <ahijazi@accsal.com>
2023-04-15 17:12:47 +0300

    ok

--- src/app/suppliers/suppliers.component.spec.ts
+++ src/app/suppliers/suppliers.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SuppliersComponent } from './suppliers.component';
+import { SuppliersComponent } from './Suppliers.component';
 
 describe('SuppliersComponent', () => {
   let component: SuppliersComponent;
