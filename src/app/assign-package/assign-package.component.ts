2b57b02e889ac282b422220f1631471ca4fe8ace
Abed Hijazi <ahijazi@accsal.com>
2024-01-09 16:12:50 +0200

    ok

--- src/app/assign-package/assign-package.component.ts
+++ src/app/assign-package/assign-package.component.ts
@@ -100,6 +100,7 @@ export class AssignPackageComponent implements OnDestroy, OnInit, AfterViewInit 
   // AH28032023
   @ViewChild('inputText') textInput: any; 
   @ViewChild('inputText1') textInput1: any; 
+  isExportExcel:boolean=false;
 // AH28032023
   public boqPackagesData: Object[] = [
     { id: 0, desc: 'All Items' },
@@ -1408,19 +1409,25 @@ export class AssignPackageComponent implements OnDestroy, OnInit, AfterViewInit 
         }
       });
     }
-  ExportExcelPackagesCost(){
-      this.assignPackageService.ExportExcelPackagesCost().subscribe((data) => {
-        if (data) {
-          let a = document.createElement('a');
-          a.id = 'downloader';
-          a.target = '_blank'; 
-          a.style.visibility = "hidden";
-          document.body.appendChild(a);
-          a.href = environment.baseApiUrl +'api/SupplierPackages/DownloadFile?filename=' + data;
-          a.click();     
-        }
-      });
-    }
+  
+    ExportExcelPackagesCost(withBoq:number){
+      let costDB=this.user.usrLoggedCostDB;
+      this.isExportExcel=true;
+  
+        this.assignPackageService.ExportExcelPackagesCost(withBoq,costDB,this.SearchInput).subscribe((data) => {
+          this.isExportExcel=false;
+          if (data) {
+            let a = document.createElement('a');
+            a.id = 'downloader';
+            a.target = '_blank'; 
+            a.style.visibility = "hidden";
+            document.body.appendChild(a);
+            a.href = environment.baseApiUrl +'api/SupplierPackages/DownloadFile?filename=' + data;
+            a.click();     
+            this.isExportExcel=false;
+          }
+        });
+      }
 
 
   private getDismissReason(reason: any): string {
